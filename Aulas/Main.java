9761ddd4797251e51e693bf0152e0a3bb599bb8a
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-01-19 10:11:30 +0000

    Changes

--- Aulas/Main.java
+++ Aulas/Main.java
@@ -1,7 +1,8 @@
 class Main{
     public static void main(String[] args){
-    Dog funcao=new Dog();
-    funcao.Ladra(); 
+    Dog novocao=new Dog();
+    novocao.Ladra(); 
+    novocao.cor2();
     Cat funcao2=new Cat();
     funcao2.Mia(); 
     }
