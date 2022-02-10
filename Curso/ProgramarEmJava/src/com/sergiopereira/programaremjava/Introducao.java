f738a5690ab210548b7f15a994a1f20972fafc02
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-02-10 16:21:45 +0000

    java

---
+++ Curso/ProgramarEmJava/src/com/sergiopereira/programaremjava/Introducao.java
@@ -0,0 +1,10 @@
+package com.sergiopereira.programaremjava;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("Eu sou o Sérgio.");
+    System.out.println("Tenho 32 anos.");
+    System.out.print("Moro no concelho de Gondomar.");
+
+  }
+}
---
+++ Curso/ProgramarEmJava/src/com/sergiopereira/programaremjava/LinkedLists_vs_ArrayLists.java
@@ -0,0 +1,2 @@
+package com.sergiopereira.programaremjava;public class LinkedLists_vs_ArrayLists {
+}
