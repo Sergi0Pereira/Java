8af37aa817220298a20acfe7fedce93337f62225
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-02-11 12:00:08 +0000

    .

--- Curso/ProgramarEmJava/src/com/sergiopereira/programaremjava/Introducao.java
+++ Curso/ProgramarEmJava/src/com/sergiopereira/programaremjava/Introducao.java
@@ -1,10 +1,26 @@
 package com.sergiopereira.programaremjava;
 
-public class Introducao {
-  public static void main(String[] args) {
-    System.out.print("Eu sou o Sérgio.");
-    System.out.println("Tenho 32 anos.");
-    System.out.print("Moro no concelho de Gondomar.");
+public class Introducao { // public significa que este codigo ficará disponivel para todas as outras classes
+
+  static String primeiroNome = "Sérgio Miguel Sousa Castro Pereira";
+  static String localidade = "Moro no concelho de Gondomar.";
+  static String idade = "Tenho 32 anos.";
+  static double PI = 3.1484378734;
+
+  public static void main(String[] args) { // funcao main é requirida em todos os programas de java que se criam
+                                           // static permite que outras classes tenham acesso
+
+    String doubletoString = Double.toString(PI);  // A string vai herdar o valor da variavel double... Pode-se fazer esta funçao com shorts,longs,int,etc..
+
+    System.out.println("Eu sou o " + primeiroNome +  ".");
+    System.out.println(idade);
+    System.out.println(localidade);
+    System.out.println("O número PI é de: "+PI + ".");
+    System.out.println(Integer.MIN_VALUE); // valor mínimo de um Integer ( .MIN_VALUE)
+    System.out.println(Integer.MAX_VALUE); // valor máximo de um Integer ( .MAX_VALUE)
+    System.out.print(doubletoString);
+
+
 
   }
 }
